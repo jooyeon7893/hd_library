c6b5fc0a5e333b353845b9699106602cf8719259
jooyeon7893 <jooyeon7893@gmail.com>
2019-11-22 15:08:04 +0900

    191122 / 수정
    section 1 - 완성
    section 4 - 완성
    section 5 - footer 완성

--- js/script.js
+++ js/script.js
@@ -31,34 +31,45 @@ $(function () {
     $grid.isotope({
       filter: filterValue
     });
-  })
+    $('#sect4 ul li').removeClass('on')
+    $(this).addClass('on')
+  });
+
+  $('#sect4 .grid-item').mouseenter(function(){
+    $('.txt').hide()
+    $(this).find('.txt').fadeIn()
+  });
+
+  $('#sect4 .grid-item').mouseleave(function(){
+    $('.txt').hide()
+  });
 
   $(window).scroll(function () {
     var top = parseInt($(this).scrollTop())
     $('h5').text(top)
-  })
+  });
 
   mouse()
-
   function mouse() {
     $('section').mousewheel(function (e, delta) {
       if (delta > 0) {
         var prev = $(this).prev().offset().top
-        $('html, body').animate({
-          'scrollTop': prev
-        })
+        $('html, body').animate({'scrollTop': prev})
       } else if (delta < 0) {
         var next = $(this).next().offset().top
-        $('html, body').animate({
-          'scrollTop': next
-        })
+        $('html, body').animate({'scrollTop': next})
       }
       $('section').off('mousewheel')
       replay()
     })
-  }
+  };
+  function replay(){setTimeout(function(){mouse()}, 500)};
 
-  function replay(){
-    setTimeout(function(){mouse()}, 500)
-  }
+  // var link = this;
+  // $.smoothScroll({
+  //   scrollTarget: link.hash,
+  //   easing: 'easeOutSine',
+  //   speed: 500
+  // });
+  
 })
