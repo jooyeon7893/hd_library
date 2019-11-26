b8fbe6e678c5a3c2b42487a3b831ba9fdd1b2a34
jooyeon7893 <jooyeon7893@gmail.com>
2019-11-26 16:17:44 +0900

    index 수정

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$(function () {
+$(function(){
   var swiper = new Swiper('.swiper-container', {
     centeredSlides: true,
     autoplay: {
@@ -19,57 +19,66 @@ $(function () {
     }
   });
 
-  $grid.isotope({
-    filter: '.talk'
-  });
-  $grid.imagesLoaded().progress(function () {
-    $grid.isotope('layout');
-  });
+  $grid.isotope({filter: '.talk'});
+
+  $grid.imagesLoaded().progress(function(){$grid.isotope('layout');});
 
   $('#sect4 ul li').click(function () {
     var filterValue = $(this).attr('data-filter');
-    $grid.isotope({
-      filter: filterValue
-    });
-    $('#sect4 ul li').removeClass('on')
-    $(this).addClass('on')
+    $grid.isotope({filter: filterValue});
+    $('#sect4 ul li').removeClass('on');;
+    $(this).addClass('on');
   });
 
   $('#sect4 .grid-item').mouseenter(function(){
     $('.txt').hide()
     $(this).find('.txt').fadeIn()
+	
+
   });
 
-  $('#sect4 .grid-item').mouseleave(function(){
-    $('.txt').hide()
-  });
+  $('#sect4 .grid-item').mouseleave(function(){$('.txt').hide();});
 
-  $(window).scroll(function () {
-    var top = parseInt($(this).scrollTop())
-    $('h5').text(top)
+  $(window).scroll(function(){
+    var top = parseInt($(this).scrollTop());
+    $('h5').text(top);
   });
 
   mouse()
-  function mouse() {
+
+  function mouse(){
     $('section').mousewheel(function (e, delta) {
       if (delta > 0) {
         var prev = $(this).prev().offset().top
-        $('html, body').animate({'scrollTop': prev})
+        $('html, body').animate({'scrollTop': prev});
       } else if (delta < 0) {
         var next = $(this).next().offset().top
-        $('html, body').animate({'scrollTop': next})
+        $('html, body').animate({'scrollTop': next});
       }
-      $('section').off('mousewheel')
+      $('section').off('mousewheel');
       replay()
-    })
+    });
   };
-  function replay(){setTimeout(function(){mouse()}, 500)};
 
-  // var link = this;
-  // $.smoothScroll({
-  //   scrollTarget: link.hash,
-  //   easing: 'easeOutSine',
-  //   speed: 500
-  // });
+  function replay(){setTimeout(function(){mouse()}, 300)};
+
+  $('#sect4 .grid-item').click(function(e){
+	  e.preventDefault()
+    $('.txt').hide();
+    var img = $(this).find('a').attr('href')	
+    $('<div class="modal_bg"><div class="modal"><img src="'+img+'"></div><a href="#" class="btn_x"><img src="img/arrow.png"></a></div>').insertBefore('#sect1');
+    $('body').css({'overflow-y':'hidden'})
+  });
   
+  // $('.btn_x').click(function(e){
+  //   alert()
+  //   e.preventDefault();
+  //   $('.modal_bg').remove()
+  // });
+
+  $(document).on('click', '.btn_x', function(e){
+    e.preventDefault()
+    $('.modal_bg').remove();
+    $('body').css({'overflow-y':'scroll'})
+  });
 })
